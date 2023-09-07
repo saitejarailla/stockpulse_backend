c4bd6f22e6255c7e0a93ed6938ea965d11306bf6
saiteja <saitejarailla@gmail.com>
2023-09-07 23:00:48 +0530

    abc

--- app.js
+++ app.js
@@ -41,30 +41,30 @@ setInterval(()=>{
   let obj = userCollectionObj.find().toArray()
 },60000*14)
 
-function checkCondition(alertCollectionObj) {
-  setInterval(async () => {
-    try {
-      const alerts = await alertCollectionObj.find().toArray();
-      for (const alert of alerts) {
-        for (const stock of stocks) {
-          if (stock.stock_name === alert.stock && stock.price === alert.price) {
-            console.log("Condition met for alert:", alert);
-            let temp = alert;
-            try {
-              await alertCollectionObj.deleteOne({
-                _id: new ObjectId(alert._id),
-              }); // Use new ObjectId to convert the ID to MongoDB ObjectId
-            } catch (error) {
-              console.log("Error deleting alert", error.message);
-            }
-          }
-        }
-      }
-    } catch (error) {
-      console.error("Error checking conditions:", error);
-    }
-  }, 6000); 
-}
+// function checkCondition(alertCollectionObj) {
+//   setInterval(async () => {
+//     try {
+//       const alerts = await alertCollectionObj.find().toArray();
+//       for (const alert of alerts) {
+//         for (const stock of stocks) {
+//           if (stock.stock_name === alert.stock && stock.price === alert.price) {
+//             console.log("Condition met for alert:", alert);
+//             let temp = alert;
+//             try {
+//               await alertCollectionObj.deleteOne({
+//                 _id: new ObjectId(alert._id),
+//               }); // Use new ObjectId to convert the ID to MongoDB ObjectId
+//             } catch (error) {
+//               console.log("Error deleting alert", error.message);
+//             }
+//           }
+//         }
+//       }
+//     } catch (error) {
+//       console.error("Error checking conditions:", error);
+//     }
+//   }, 6000); 
+// }
 
 app.use(express.json());
 app.use(
