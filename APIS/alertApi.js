a726c6744f13b27ce09805e48c6a8b69cee903bf
saiteja <saitejarailla@gmail.com>
2023-09-07 23:10:14 +0530

    ab

--- APIS/alertApi.js
+++ APIS/alertApi.js
@@ -5,51 +5,27 @@ const expressAsyncHandler = require("express-async-handler");
 
 alertApp.use(express.json());
 
+
+
+
 alertApp.post(
-  "/set-alert",
-  expressAsyncHandler(async (request, response) => {
+"/set-alert",
+expressAsyncHandler(async (request, response) => {
     let obj = request.body;
     let alertCollectionObj = request.app.get("alertCollectionObj");
     await alertCollectionObj.insertOne(obj);
     response.send({ message: "alert set successfully" });
-    function checkCondition(alertCollectionObj) {
-      setInterval(async () => {
-        try {
-            console.log(".")
-          const alerts = await alertCollectionObj.find().toArray();
-          for (const alert of alerts) {
-            for (const stock of stocks) {
-              if (
-                stock.stock_name === alert.stock &&
-                stock.price === alert.price
-              ) {
-                console.log("Condition met for alert:", alert);
-                let temp = alert;
-                try {
-                  await alertCollectionObj.deleteOne({
-                    _id: new ObjectId(alert._id),
-                  }); // Use new ObjectId to convert the ID to MongoDB ObjectId
-                } catch (error) {
-                  console.log("Error deleting alert", error.message);
-                }
-              }
-            }
-          }
-        } catch (error) {
-          console.error("Error checking conditions:", error);
-        }
-      }, 6000);
-    }
-  })
+    
+})
 );
 
 alertApp.get(
-  "/get-alert",
-  expressAsyncHandler(async (request, response) => {
+    "/get-alert",
+    expressAsyncHandler(async (request, response) => {
     let alertCollectionObj = request.app.get("alertCollectionObj");
     let alerts = await alertCollectionObj.find().toArray();
     response.send({ message: "alerts data", payload: alerts });
-  })
+})
 );
 
 module.exports = alertApp;
